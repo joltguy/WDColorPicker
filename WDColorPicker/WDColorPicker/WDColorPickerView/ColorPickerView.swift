e135a68232e658c0b554223af51d335008f64ab0
Jeff Marlow <jeff@joltguy.com>
2017-11-02 13:00:04 -0400

    Added property observers to prevent setting out-of-range values for hue, saturation, and brightness.

--- WDColorPicker/WDColorPicker/WDColorPickerView/ColorPickerView.swift
+++ WDColorPicker/WDColorPicker/WDColorPickerView/ColorPickerView.swift
@@ -21,9 +21,21 @@ class ColorPickerView: UIView {
     var colorPickTapGesture:UITapGestureRecognizer?
     var colorPickPanGesture:UIPanGestureRecognizer?
 
-    var hue:CGFloat = 0.0
-    var saturation:CGFloat = 0.0
-    var brightness:CGFloat = 0.0
+	var hue: CGFloat = 0.0 {
+		didSet {
+			self.hue = min(max(self.hue, 0.0), 1.0)
+		}
+	}
+	var saturation: CGFloat = 0.0 {
+		didSet {
+			self.saturation = min(max(self.saturation, 0.0), 1.0)
+		}
+	}
+	var brightness: CGFloat = 0.0 {
+		didSet {
+			self.brightness = min(max(self.brightness, 0.0), 1.0)
+		}
+	}
     var initialized = false
     
     
